852bafed48d81b1b3844c49287c38a5d3bddebb5
Huzaifa Nasir <huzaifanasir.0270@gmail.com>
2026-07-15 19:09:40 +0500

    Delete schema_check.php

--- schema_check.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'config/functions.php';
-$res = $conn->query("DESCRIBE announcements");
-$cols = [];
-while($row = $res->fetch_assoc()){
-    $cols[] = $row;
-}
-echo json_encode($cols, JSON_PRETTY_PRINT);
